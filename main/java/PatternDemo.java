95d0d52d40613d3254abad039f89853efb27a735
shuban0911 <uma.sankari09@gmail.com>
2023-05-24 14:26:38 -0400

    Added a comment

--- main/java/PatternDemo.java
+++ main/java/PatternDemo.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PatternDemo {
-
+//Added a commit
 	public static void main(String[] args) {
 		Pattern p = Pattern.compile("a-z");
 		Matcher matcher = p.matcher("Shuban");
