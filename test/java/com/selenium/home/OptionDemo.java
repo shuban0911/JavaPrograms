476f5600af074690f55c26cb86662fd34202e1eb
shuban0911 <uma.sankari09@gmail.com>
2023-05-29 16:08:00 -0400

    added a comment

--- test/java/com/selenium/home/OptionDemo.java
+++ test/java/com/selenium/home/OptionDemo.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OptionDemo {
+	//going to add reviewers
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
